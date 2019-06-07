ad34557f8b4cbc305e2e43f89cbfd2a90e0d39c0
Riccardo Scalco <riccardo.scalco@gmail.com>
2019-06-07 08:27:25 +0200

    v1.0.0

--- src/style.js
+++ src/style.js
@@ -1,11 +1,7 @@
-import { html } from 'https://unpkg.com/hybrids/src';
+import { html } from 'hybrids';
 
 export default html`
 	<style>
-
-		svg {
-			border: 1px solid red;
-		}
 
 		svg .border {
 			stroke: #353839;
@@ -25,14 +21,26 @@ export default html`
 			stroke-width: 2px;
 		}
 
+		svg text {
+			font-family: monospace;
+			font-family: var(--font-family);
+			fill: #353839;
+		}
+
 		svg .labels text {
+			font-size: 10px;
 			text-anchor: middle;
 		}
 
 		svg .titles text {
-			font-size: 13px;
+			font-size: 12px;
+			font-size: var(--font-size);
 			font-weight: bold;
 			text-anchor: middle;
+		}
+
+		svg .data circle {
+			fill: #353839;
 		}
 
 	</style>
